0604dae8f7f833442c57d315867e1b96cd872cac
kanghy22 <nasmurf1hk@gmail.com>
2018-05-10 16:50:19 -0400

    control paddle complete

--- scripts/pong.js
+++ scripts/pong.js
@@ -1,115 +1,218 @@
-class Vec
-{
-    constructor(x = 0, y = 0)
-    {
-        this.x = x;
-        this.y = y;
+// START - General Animation and Canvas definitions and refactoring of code
+
+	// animation framerate
+	var animate = window.requestAnimationFrame ||
+	  window.webkitRequestAnimationFrame ||
+	  window.mozRequestAnimationFrame ||
+	  function(callback) { window.setTimeout(callback, 1000/60) };
+
+	// create the canvas
+	var canvas = document.createElement('canvas');
+
+	// function to get window width
+	var width = function getActualWidth() {
+	    var actualWidth = window.innerWidth ||
+	                      document.documentElement.clientWidth ||
+	                      document.body.clientWidth ||
+	                      document.body.offsetWidth;
+
+	    return actualWidth;
+	};
+
+	// function to get window height
+	var height = function getActualHeight() {
+	    var actualHeight = window.innerHeight ||
+	                      document.documentElement.clientHeight ||
+	                      document.body.clientHeight ||
+	                      document.body.offsetHeight;
+
+	    return actualHeight;
+	};
+
+	// define canvas width and height
+	canvas.width = width();
+	canvas.height = height();
+
+	// canvas context for 2D
+	var context = canvas.getContext('2d');
+
+	// create the canvas when window loads and begin animating
+	window.onload = function() {
+		document.body.appendChild(canvas);
+		animate(step);
+	};
+
+	// game function calls for continuous animation
+	var step = function() {
+		update();
+		render();
+		animate(step);
+	};
+
+// END - General Animation and Canvas definitions
+
+// START - Game Asset Definitions and Rendering
+
+	// define a game paddle
+	function Paddle(x, y, width, height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.x_speed = 0;
+		this.y_speed = 0;
+	};
+
+	// add render method to paddle prototype
+	Paddle.prototype.render = function() {
+		context.fillStyle = "white";
+		context.fillRect(this.x, this.y, this.width, this.height);
+	};
+
+	// define player and computer paddles
+	function Player() {
+		this.paddle = new Paddle(.98*width(), .4*height() , .01*width(), .2*height());
+	};
+
+	function Computer() {
+		this.paddle = new Paddle(.01*width(), .4*height(), .01*width(), .2*height());
+	};
+
+	// add render methods to player and computer prototypes which render paddles
+	Player.prototype.render = function() {
+		this.paddle.render();
+	};
+
+	Computer.prototype.render = function() {
+		this.paddle.render();
+	};
+
+	// define game ball
+	function Ball(x, y) {
+		this.radius = .01*canvas.width;
+		this.x = x;
+		this.y = y;
+		this.x_speed = Math.random() >= .5 ? .5*this.radius : -.5*this.radius;
+		this.y_speed = 0;
+	};
+
+	// add render method to ball prototype
+	Ball.prototype.render = function() {
+		context.beginPath();
+		context.arc(this.x, this.y, this.radius, 0*Math.PI, 2*Math.PI, false);
+		context.fillStyle = "white";
+		context.fill();
+	};
+
+// END - Game Asset Definitions and Rendering
+
+// START - Game Asset Update Mechanisms
+
+	// game asset updates
+	var update = function() {
+		player.update();
+		ball.update(player.paddle, computer.paddle);
+	};
+
+	// player position updating / movement
+	Player.prototype.update = function() {
+		for(var key in keysDown) {
+			var value = Number(key);
+			// up arrow
+			if(value == 38) {
+				// paddle moves by 10% of paddle height
+				this.paddle.move(0, -(.02*canvas.height));
+			// down arrow
+			} else if(value == 40) {
+				this.paddle.move(0, .02*canvas.height);
+			} else {
+				this.paddle.move(0, 0);
+			}
+		}
+	};
+
+
+
+	// paddle movement
+	Paddle.prototype.move = function(x, y) {
+	  	this.x += x;
+		this.y += y;
+		this.x_speed = x;
+		this.y_speed = y;
+		if(this.y < 0) {
+			// paddle moves off screen to the top
+			this.y = 0;
+			this.y_speed = 0;
+		} else if(this.y + this.height > canvas.height) {
+			// paddle moves off screen to the bottom
+			this.y = canvas.height - this.height;
+			this.y_speed = 0;
+		}
+	};
+
+	// ball position updating / movement
+	Ball.prototype.update = function(paddle1, paddle2) {
+		this.x += this.x_speed;
+		this.y += this.y_speed;
+		// ball definition
+		var top_x = this.x - this.radius;
+		var top_y = this.y - this.radius;
+		var bottom_x = this.x + this.radius;
+		var bottom_y = this.y + this.radius;
+
+		if(this.y - this.radius < 0) { // hitting the top wall
+		  this.y = this.radius;
+		  this.y_speed = -this.y_speed;
+		} else if(this.y + this.radius > height()) { // hitting the bottom wall
+		  this.y = height() - this.radius;
+		  this.y_speed = -this.y_speed;
+		}
+
+		if(this.x < 0 || this. x > width()) { // point was scored
+		  this.x_speed = Math.random() >= .5 ? .5*this.radius : -.5*this.radius;
+		  this.y_speed = 0;
+		  this.x = canvas.width/2 - this.radius;
+		  this.y = canvas.height/2 - this.radius;
+		}
+	};
+
+// END - Game Asset Update Mechanisms
+
+// START - Interaction and Initialization
+
+	// define the interaction keys
+	var keysDown = {};
+
+	// when a key is pressed
+	window.addEventListener('keydown', function(event) {
+		keysDown[event.keyCode] = true;
+	});
+
+	// when a key is let go
+	window.addEventListener('keyup', function(event) {
+		delete keysDown[event.keyCode];
+	});
+
+  //remove scrolling when keys are pressed
+  window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
     }
-}
+}, false);
 
-class Rect
-{
-    constructor(a, b)
-    {
-        this.pos = new Vec;
-        this.size = new Vec(a, b);
-    }
-    get left()
-    {
-        return this.pos.x - this.size.x / 2;
-    }
-    get right()
-    {
-        return this.pos.x + this.size.x / 2;
-    }
-    get top()
-    {
-        return this.pos.y - this.size.y / 2;
-    }
-    get bottom()
-    {
-        return this.pos.y + this.size.y / 2;
-    }
-}
+	// game asset calls
+	var player = new Player();
+	var computer = new Computer();
+	var ball = new Ball((canvas.width/2 - .01*canvas.width), (canvas.height/2 - .01*canvas.height));
 
-class Ball extends Rect
-{
-    constructor()
-    {
-        super(10, 10);
-        this.vel = new Vec;
-    }
-}
+	// render the board and the game assets
+	var render = function() {
+		context.fillStyle = "black";
+		context.fillRect(0, 0, width(), height());
+		player.render();
+		computer.render();
+		ball.render();
+	};
 
-class Player extends Rect
-{
-    constructor()
-    {
-        super(20, 100);
-        this.score = 0;
-    }
-}
-
-class Pong
-{
-    constructor(canvas)
-    {
-        this._canvas = canvas;
-        this._context = canvas.getContext('2d');
-        this.ball = new Ball;
-        this.ball.pos.x = 100;
-        this.ball.pos.y = 50;
-
-        this.ball.vel.x = 100;
-        this.ball.vel.y = 100;
-
-        this.players = [
-            new Player,
-            new Player,
-        ];
-
-        this.players[0].pos.x = 40;
-        this.players[1].pos.x = this._canvas.width - 40;
-
-        this.players.forEach(player => {
-          player.pos.y = this._canvas.height / 2; 
-        });
-
-        let lastTime;
-        const callback = (millis) =>  {
-            if (lastTime) {
-                this.update((millis - lastTime) / 1000)
-            }
-            lastTime = millis;
-            requestAnimationFrame(callback);
-        };
-        callback();
-    }
-    draw() {
-        this._context.fillStyle = '#000';
-        this._context.fillRect(0, 0, this._canvas.width, this._canvas.height);
-
-        this.drawRect(this.ball);
-        this.players.forEach(player => this.drawRect(player));
-    }
-    drawRect(rect) {
-        this._context.fillStyle = '#fff';
-        this._context.fillRect(rect.pos.x, rect.pos.y, rect.size.x, rect.size.y);
-    }
-    update(dt) {
-        this.ball.pos.x += this.ball.vel.x * dt;
-        this.ball.pos.y += this.ball.vel.y * dt;
-
-        if (this.ball.left < 0 || this.ball.right > this._canvas.width) {
-            this.ball.vel.x = -this.ball.vel.x;
-        }
-        if (this.ball.top < 0 || this.ball.bottom > this._canvas.height) {
-            this.ball.vel.y = -this.ball.vel.y;
-        }
-
-        this.draw();
-    }
-}
-
-const canvas = document.getElementById('pong');
-const pong = new Pong(canvas);
+// END - Interaction and Initialization
